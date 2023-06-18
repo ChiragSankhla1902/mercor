8f439bdcc9471a0cd0284d88b8800afb559a0c53
Gurleen Kaur <gurleen.k@jeevansathi.com>
2023-06-18 18:19:10 +0530

    navbar and sidebar

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import './App.css'
+import Navbar from './componenets/Navbar'
+import Side from './componenets/Side'
+import Dnd from './componenets/Dnd'                                   
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Side />
+      <Dnd />
+    </>
+  )
+}
+
+export default App
---
+++ componenets/Dnd.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Dnd.css'
+
+
+export default function Dnd() {
+    return (
+        <div className='Container'>
+            <div className='Title'> fhjrfhj</div>
+            hjfjgdh
+        </div>
+    )
+}
+
+
+
